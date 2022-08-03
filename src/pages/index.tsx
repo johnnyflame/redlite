f57afcaa59cf48d14b3fb6117ee40f7739183599
johnnyflame <johnnyflamelee@gmail.com>
2022-08-03 13:57:36 +0200

    tutorial 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+          <p>Ass kicking rock band from Stockholm, Sweden </p>
+          <StaticImage
+        alt="redlite band logo"
+        src="../images/redlite02_neon.png"
+          />
+          <StaticImage
+        alt="crowd"
+        src="../images/crowd.jpeg"
+      />
     </Layout>
   )
 }
