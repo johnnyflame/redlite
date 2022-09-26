ed567cce3ab220f6937d70bb0d352a326489dea0
johnnyflame <johnnyflamelee@gmail.com>
2022-09-27 01:18:52 +0200

    using bootstrap to make navbar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,58 +1,62 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
-import {
-  container,
-  heading,
-  navLinks,
-  navLinkItem,
-} from './layout.module.css'
+import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.css';
+import Stack from 'react-bootstrap/Stack';
+
+
 import { SocialIcon } from 'react-social-icons';
 
 import { StaticImage } from 'gatsby-plugin-image'
 
+
+
 const Layout = ({ pageTitle, children }) => {
 
-  const header = <header >
-    <nav className="flex items-center justify-between flex-wrap bg-white-500 p-6">
-      <div className="flex flex-shrink-0 text-white mr-6">
-        <StaticImage
-          alt="redlite band logo"
-          src="../images/logo.png"
-        />
+  const header =
+    <Navbar collapseOnSelect expand='sm' bg='dark' variant='dark'>
+
+      <Container>
+        <div>
+          <Navbar.Brand href="/"> <StaticImage
+            alt="redlite band logo"
+            src="../images/logo.png"
+            width={300}
+          /></Navbar.Brand>
+        </div>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav' />
+        <Nav className="me-auto">
+          <Nav.Link href='/'>Home</Nav.Link>
+          <Nav.Link href='/about'>About</Nav.Link>
+          <Nav.Link href='/events'>events</Nav.Link>
+          <Nav.Link href='/contact'>Contact</Nav.Link>
+        </Nav>
+      </Container>
+
+      <div>
+        <SocialIcon url="https://www.instagram.com/redliteband/?hl=en" bgColor='#FFFFFF' />
+        <SocialIcon url="https://www.facebook.com/redliteband" bgColor='#FFFFFF' />
+        <SocialIcon url="https://www.youtube.com/channel/UCbz_O3jnCLccERCM3BF5X0g" bgColor='#FFFFFF' />
+        <SocialIcon url="https://open.spotify.com/artist/2jKjTM6wl6CiHrBakMReOX?si=Yb-_Nyb9Qsq4j2Ua4QCztA" bgColor='#FFFFFF' />
       </div>
-      <ul className={navLinks}>
-        <li className={navLinkItem}>
-          <Link to="/">HOME</Link></li>
-        <li className={navLinkItem}>
-          <Link to="/about">BIO</Link></li>
+    </Navbar>
 
-        <li className={navLinkItem}>
-          <Link to="/events">EVENTS</Link></li>
-
-        <li className={navLinkItem}>
-          <Link to="/contact">CONTACT</Link></li>
-
-      </ul>
-    </nav>
-  </header>
   return (
-    <div className={container}>
-      <title>{pageTitle}</title>
-      <div className="flex flex-col h-screen justify-between">
+    <div >
+      {/* <title>{pageTitle}</title> */}
 
-        {header}
-        <main>
-          <h1 className={heading}>{pageTitle}</h1>
-          {children}
-        </main>
+      {header}
+      <main>
+        {/* <h1 >{pageTitle}</h1> */}
+        {children}
+      </main>
 
-        <footer className="h-10">
+      <footer className="h-10">
 
-          {/* https://jaketrent.github.io/react-social-icons/ */}
-          <SocialIcon url="https://www.instagram.com/redliteband/?hl=en" />
-          <SocialIcon url="https://www.facebook.com/redliteband" />
-        </footer>
-      </div>
+        {/* https://jaketrent.github.io/react-social-icons/ */}
+
+      </footer>
+      {/* </div> */}
 
       {/* <div className="flex flex-col h-screen justify-between">
         <header className="h-10 bg-red-500">Header</header>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,60 @@ import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
 import Spotify from 'react-spotify-embed'
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.css';
+import Container from 'react-bootstrap/Container';
+
+import Stack from 'react-bootstrap/Stack';
+
+
+
+// function BandImageCarousel() {
+//   return (<Carousel fade  >
+//     <Carousel.Item>
+//       <StaticImage
+//         alt="redlite band"
+//         src="../images/playing_1.jpg"
+//         width={1300}
+//         height={900}
+//       />
+//       <Carousel.Caption>
+//         <h3>First slide label</h3>
+//         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+//       </Carousel.Caption>
+//     </Carousel.Item>
+//     <Carousel.Item>
+//       <StaticImage
+//         alt="redlite band"
+//         src="../images/redlite_03.jpeg"
+//         width={1300}
+//       // height={900}
+//       />
+//       <Carousel.Caption>
+//         <h3>Second slide label</h3>
+//         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+//       </Carousel.Caption>
+//     </Carousel.Item>
+//     <Carousel.Item>
+//       <StaticImage
+//         alt="redlite band"
+//         src="../images/band.jpg"
+//         width={1300}
+//         height={900}
+//       />
+//       <Carousel.Caption>
+//         <h3>Third slide label</h3>
+//         <p>
+//           Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+//         </p>
+//       </Carousel.Caption>
+//     </Carousel.Item>
+//   </Carousel>
+//   );
+// }
+
+
+
 
 const IndexPage = () => {
 
@@ -10,23 +64,23 @@ const IndexPage = () => {
 
 
   return (
-    <Layout pageTitle="Home">
+    <Layout pageTitle="RËDLITE OFFICIAL">
+
       {/* <p>Energetic hard rock band from Stockholm, Sweden </p> */}
-      <StaticImage
-        alt="redlite band"
-        src="../images/band.jpg"
-      />
+      <Container fluid className='gx-0'>
+        <Stack gap={3}>
+          <StaticImage
+            alt="redlite band"
+            src="../images/band.jpg"
+          />
 
-      <StaticImage
-        alt="a crowd"
-        src="../images/stage.jpeg"
+          <Container>Who are we</Container>
+          <Container>Our Music</Container>
+          {biteMyBullet}
 
-      />
-      {biteMyBullet}
-
-      <iframe src="https://open.spotify.com/embed/artist/2jKjTM6wl6CiHrBakMReOX?utm_source=generator&theme=0" width="100%" height="380" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-
-
+          <iframe src="https://open.spotify.com/embed/artist/2jKjTM6wl6CiHrBakMReOX?utm_source=generator&theme=0" width="100%" height="380" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </Stack>
+      </Container>
     </Layout>
   )
 }
